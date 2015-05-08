fd8ed6edcb4c8f3881261b76ddca10a5c22e91af
fcazorlay <fcazorlay@gmail.com>
2015-05-08 19:36:23 +0200

    Sirena
    Exercici 3 AIO

--- Sirena/Sirena.ino
+++ Sirena/Sirena.ino
@@ -2,14 +2,14 @@
 **                                                                               **
 **                                  Sirena                                       **
 **                                                                               **
-**                                 Programa1                                     **
+**                                                                     **
 **                                                                               **
 **********************************************************************************/
 
 //********** Includes *************************************************************
 
 //********** Variables ************************************************************
-const int xiulet = 9:        // donar nom al pin 9 de l’Arduino
+const int xiulet = 9;        // donar nom al pin 9 de l’Arduino
 const int pot0 = A0;         // donar nom al pin A0 de l’Arduino
 int valPot0;                 // guardar valor del potènciometre  
 
